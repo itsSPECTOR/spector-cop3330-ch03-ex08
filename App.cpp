7673e1d0cc47c4e8d73f9e3bf5eb1deb18bf3c05
Reese Spector <89818858+itsSPECTOR@users.noreply.github.com>
2021-11-25 12:13:51 -0500

    Add files via upload

---
+++ App.cpp
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Spector Reese
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //initialize input
+	int input = 0;
+	
+	//prompt user
+	cout << "Enter your number: \n";
+	
+	//save user input to 'input' variable
+	cin >> input;
+	
+	//if modulo 2 = 0, it's even. If else, it's odd
+	if (input % 2 == 0) {
+		cout << input << " is even.\n";
+	} else {
+		cout << input << " is odd.\n";
+	}
+	
+return 0;
+}
